486aa702dc0029e639cf8ffa1cd465df04ee1162
Mathias Nielsen <mathias252220@gmail.com>
2024-04-26 08:15:24 +0200

    Work in progress

--- SkattejagtGeneratorWeb/Pages/SkattejagtGenerator/SkattejagtGenerator.cshtml.cs
+++ SkattejagtGeneratorWeb/Pages/SkattejagtGenerator/SkattejagtGenerator.cshtml.cs
@@ -5,7 +5,14 @@ namespace SkattejagtGeneratorWeb.Pages.SkattejagtGenerator
 {
     public class SkattejagtGeneratorModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public int numberOfOutposts { get; set; } = 1;
+        [BindProperty]
+        public List<string> outposts { get; set; } = new List<string>();
         public void OnGet()
+        {
+		}
+        public IActionResult OnPost()
         {
         }
     }
