ea5552e977670e7d532734068b28d07d4b2de619
Mathias Nielsen <mathias252220@gmail.com>
2024-05-27 23:11:47 +0200

    Work in progress

---
+++ SkattejagtGeneratorWebApp/wwwroot/Scripts/DownloadFile.js
@@ -0,0 +1,10 @@
+﻿<script>
+    window.downloadFile = (fileName, pdfData) => {
+        const linkSource = `data:application/pdf;base64,${pdfData}`;
+        const downloadLink = document.createElement("a");
+        downloadLink.href = linkSource;
+        downloadLink.download = fileName;
+        downloadLink.click();
+        downloadLink.remove();
+    }
+</script>
