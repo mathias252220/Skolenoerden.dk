783c3fbd477603b5e410b086f43031274d31d66c
Mathias Nielsen <mathias252220@gmail.com>
2024-05-30 08:55:09 +0200

    Updated groups and outpost buttons. Min 1, max 10 in both

--- LogicLibrary/Models/OutpostModel.cs
+++ LogicLibrary/Models/OutpostModel.cs
@@ -1,6 +1,7 @@
 ﻿using LogicLibrary.Modeller;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace LogicLibrary.Models
 {
     public class OutpostModel
     {
+        [Required(ErrorMessage = "En eller flere af dine poster mangler et navn")]
         public string Name { get; set; }
         public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
         public string ReturnNameUnderscored()
