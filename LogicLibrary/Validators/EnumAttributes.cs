fce3971a8e80fbb79ac1351b93266c93e0398a37
Mathias Nielsen <mathias252220@gmail.com>
2024-09-25 07:50:15 +0200

    Added validation to task generator

---
+++ LogicLibrary/Validators/EnumAttributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QuestPDF.Helpers.Colors;
+
+namespace LogicLibrary.Validators;
+public class EnumNotZeroAttribute : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        int intValue = (int)value;
+
+        return intValue != 0;
+    }
+}
