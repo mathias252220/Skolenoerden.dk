1801c00d8603586d3d5b36907d2db6d4275b3149
mathias252220 <mathias252220@gmail.com>
2025-10-30 17:29:51 +0100

    TerraformerUpdate 0.9

--- SkolenoerdenWEB/Components/API/Controllers/TerraformerController.cs
+++ SkolenoerdenWEB/Components/API/Controllers/TerraformerController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.IO;
+using System.Text.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -38,41 +39,44 @@ public class TerraformerController : ControllerBase
 
     // POST api/Terraformer
     [HttpPost]
-    public void Post([FromBody] JsonContent gameSave)
+    [Consumes("application/json")]
+    public string Post([FromBody] JsonElement gameSaveRaw)
     {
-        System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/lastAccessedGame2.json", gameSave.ToString());
+        var gameSaveJson = gameSaveRaw.GetRawText();
+        GameSaveModel gameSave = JsonConvert.DeserializeObject<GameSaveModel>(gameSaveJson);
 
-        //string returnString;
+        string returnString;
 
-        //string jsonString = System.IO.File.ReadAllText(wwwrootPath + "/TerraformerSaveData/gameNames.json");
-        //GameNameListModel gameNameList = JsonConvert.DeserializeObject<GameNameListModel>(jsonString);
+        string jsonString = System.IO.File.ReadAllText(wwwrootPath + "/TerraformerSaveData/gameNames.json");
+        GameNameListModel gameNameList = JsonConvert.DeserializeObject<GameNameListModel>(jsonString);
 
-        //if (!gameNameList.gameNames.Contains(gameSave.gameCode))
-        //{
-        //    gameNameList.gameNames.Add(gameSave.gameCode);
-        //    string updatedGameNamesJson = JsonConvert.SerializeObject(gameNameList, Formatting.Indented);
-        //    System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/gameNames.json", updatedGameNamesJson);
-        //    returnString = "GameCreated";
-        //}
-        //else
-        //{
-        //    string oldGameSaveJson = System.IO.File.ReadAllText(wwwrootPath + "/TerraformerSaveData/" + gameSave.gameCode + ".json");
-        //    GameSaveModel oldGameSave = JsonConvert.DeserializeObject<GameSaveModel>(oldGameSaveJson);
+        if (!gameNameList.gameNames.Contains(gameSave.gameCode))
+        {
+            gameNameList.gameNames.Add(gameSave.gameCode);
+            string updatedGameNamesJson = JsonConvert.SerializeObject(gameNameList, Formatting.Indented);
+            System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/gameNames.json", updatedGameNamesJson);
+            System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/" + gameSave.gameCode + ".json", gameSaveJson);
+            returnString = "GameCreated";
+        }
+        else
+        {
+            string oldGameSaveJson = System.IO.File.ReadAllText(wwwrootPath + "/TerraformerSaveData/" + gameSave.gameCode + ".json");
+            GameSaveModel oldGameSave = JsonConvert.DeserializeObject<GameSaveModel>(oldGameSaveJson);
 
-        //    if (gameSave.gameMasterCode == oldGameSave.gameMasterCode)
-        //    {
-        //        string newGameSaveJson = JsonConvert.SerializeObject(gameSave, Formatting.Indented);
-        //        System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/" + gameSave.gameCode + ".json", newGameSaveJson);
-        //        returnString = "GameSaved";
-        //    }
-        //    else
-        //    {
-        //        returnString = "WrongGameMasterCodeOrGameExists";
-        //    }
+            if (gameSave.gameMasterCode == oldGameSave.gameMasterCode)
+            {
+                string newGameSaveJson = JsonConvert.SerializeObject(gameSave, Formatting.Indented);
+                System.IO.File.WriteAllText(wwwrootPath + "/TerraformerSaveData/" + gameSave.gameCode + ".json", gameSaveJson);
+                returnString = "GameSaved";
+            }
+            else
+            {
+                returnString = "WrongGameMasterCodeOrGameExists";
+            }
 
-        //}
+        }
 
-        //return returnString;
+        return returnString;
 
     }
 }
